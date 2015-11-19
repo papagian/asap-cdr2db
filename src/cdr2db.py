273ad73d8c4b00149650c166ce2acbc840564cdf
Sofia Papagiannaki <papagian@gmail.com>
2015-11-19 17:18:03 +0200

    Changes for new CDR input data format

--- src/cdr2db.py
+++ src/cdr2db.py
@@ -5,16 +5,35 @@ import sys
 
 from progress.spinner import Spinner
 
-from sqlalchemy import create_engine, Table, Column, Integer, MetaData, String
+from sqlalchemy import create_engine, Table, Column, Integer, MetaData, String, DateTime
 
 def prepate_db(conn_str):
      engine = create_engine(conn_str)
      metadata = MetaData()
-     t = Table('cdr', metadata,
-                 Column('id', String),
-                 Column('hour', Integer),
-                 Column('dow', Integer),
-                 Column('doy', Integer))
+     t = Table('cdr',
+               metadata,
+               Column('cdrType', String),
+               Column('callingPartyNumberKey', String),
+               Column('callingSubscriberImsi', String),
+               Column('dateTime', DateTime),
+               Column('chargeableDuration', Integer),
+               Column('exchangeIdentity', String),
+               Column('outgoingRoute', String),
+               Column('incomingRoute', String),
+               Column('cellId1stCellCalling', String),
+               Column('gsmTeleServiceCode', String),
+               Column('cellIdLastCellCalling', String),
+               Column('disconnectingParty', String),
+               Column('callingSubscriberImei', String),
+               Column('tac', String),
+               Column('residentCustomerFlag', String),
+               Column('paymentType', String),
+               Column('contractStatus', String),
+               Column('contractStartingDate', String),
+               Column('country', String),
+               Column('cityPostalCode', String),
+               Column('city', String))
+
      metadata.create_all(engine)
      return engine.connect(), t
 
@@ -22,7 +41,6 @@ def prepate_db(conn_str):
 def extract_cdr_data(f):
     spinner = Spinner('Loading %s:' % f)
     with open(f) as fd:
-        fd.readline()  # omit the headline
         while 1:
             l = fd.readline()
             spinner.next()
@@ -30,15 +48,36 @@ def extract_cdr_data(f):
                 raise StopIteration
             if l == '':
                 continue
-            separator = ';'  # new CDR formt
-            t = l.split(separator)
-            d = datetime.datetime.strptime(':'.join([t[3], t[4]]),  # new CDR format
-                                           '%Y%m%d:%H%M%S')
-            dt = d.timetuple()
-            yield {'id': t[9],  # new CDR format
-                   'hour': dt.tm_hour,
-                   'dow': (dt.tm_wday + 2) % 7, # normalize to conform java.util.Calendar
-                   'doy': dt.tm_yday}
+            t = l.split(';')
+            try:
+                d = datetime.datetime.strptime(':'.join([t[3], t[4]]),
+                                            '%Y%m%d:%H%M%S')
+            except:
+                continue
+            else:
+                yield {
+                    'cdrType': t[0],
+                    'callingPartyNumberKey': t[1],
+                    'callingSubscriberImsi': t[2],
+                    'dateTime': d,
+                    'chargeableDuration': int(t[5]),
+                    'exchangeIdentity': t[6],
+                    'outgoingRoute': t[7],
+                    'incomingRoute': t[8],
+                    'cellId1stCellCalling': t[9],
+                    'gsmTeleServiceCode': t[10],
+                    'cellIdLastCellCalling': t[11],
+                    'disconnectingParty': t[12],
+                    'callingSubscriberImei': t[13],
+                    'tac': t[14],
+                    'residentCustomerFlag': t[15],
+                    'paymentType': t[16],
+                    'contractStatus': t[17],
+                    'contractStartingDate': t[18],
+                    'country': t[19],
+                    'cityPostalCode': t[20],
+                    'city': t[21]
+                }
 
 
 def traverse_cdr(path, conn, insert_stmt):
